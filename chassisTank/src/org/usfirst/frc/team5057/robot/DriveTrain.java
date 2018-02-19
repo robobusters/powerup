4430b0c1a360ad53fa150ab685bab191a69f621b
abhimaster2001 <abhimaster2001@gmail.com>
2018-02-19 13:25:02 -0600

    hcfj

---
+++ chassisTank/src/org/usfirst/frc/team5057/robot/DriveTrain.java
@@ -0,0 +1,172 @@
+package org.usfirst.frc.team5057.robot;
+
+import edu.wpi.first.wpilibj.ADXRS450_Gyro;
+import edu.wpi.first.wpilibj.AnalogInput;
+import edu.wpi.first.wpilibj.BuiltInAccelerometer;
+import edu.wpi.first.wpilibj.GenericHID.Hand;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.interfaces.Accelerometer;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public class DriveTrain {
+	double leftY;
+	double leftX;
+	double rightY;
+	double rightX;
+	double dampen;
+	boolean dpUp;
+	boolean dpDown;
+	boolean dpLeft;
+	boolean dpRight;
+	int mode;
+	
+	DifferentialDrive chassis;
+	XboxController xbox;
+	JoystickLocations porting;
+	
+	private static final int kUltrasonicPort1 = 0;
+	private AnalogInput m_ultrasonic1 = new AnalogInput(kUltrasonicPort1);
+	private static final int kUltrasonicPort2 = 2;
+	private AnalogInput m_ultrasonic2 = new AnalogInput(kUltrasonicPort2);
+	private static final double kValueToInches = 0.049;
+	
+	ADXRS450_Gyro gyro = new ADXRS450_Gyro();
+	Accelerometer accel= new BuiltInAccelerometer(Accelerometer.Range.k4G);
+	
+	double currentDistance1;
+	double currentDistance2;
+	double[] accVal = new double[3];
+	
+	public DriveTrain(DifferentialDrive d,XboxController x, JoystickLocations p) {
+		chassis=d;
+		xbox=x;
+		porting=p;
+	}
+	
+	public void updateAxes() {
+		leftY=-xbox.getRawAxis(porting.lYAxis);
+		leftX=xbox.getRawAxis(porting.lXAxis);
+		rightX=xbox.getRawAxis(porting.rXAxis);
+		rightY=xbox.getRawAxis(porting.rYAxis);
+		dampen=1-3/4*xbox.getTriggerAxis(Hand.kRight);
+		SmartDashboard.putNumber("leftY", leftY);
+		SmartDashboard.putNumber("leftX", leftX);
+		SmartDashboard.putNumber("rightY", rightY);
+		SmartDashboard.putNumber("rightX", rightX);
+		SmartDashboard.putNumber("dampen", dampen);
+		
+		if(Math.round(xbox.getPOV())!=1) {
+			if(Math.round(xbox.getPOV())<90) {
+				dpUp = true;
+				dpDown = false;
+				dpLeft = false;
+				dpRight = false;
+			}else if(Math.round(xbox.getPOV())<180) {
+				dpUp = false;
+				dpDown = false;
+				dpLeft = false;
+				dpRight = true;
+			}else if(Math.round(xbox.getPOV())<270) {
+				dpUp = false;
+				dpDown = true;
+				dpLeft = false;
+				dpRight = false;
+			}else if(Math.round(xbox.getPOV())<360) {
+				dpUp = false;
+				dpDown = false;
+				dpLeft = true;
+				dpRight = false;
+			}
+		}else {
+			
+				dpUp = false;
+				dpDown = false;
+				dpLeft = false;
+				dpRight = false;
+			
+		}
+	}
+	
+
+	//This changes which kind of drive system to use. Ignore for now
+	public void changeDrive() {
+		if(xbox.getAButton())mode=1;
+		else if(xbox.getBButton())mode=2;
+		else if(xbox.getXButton())mode=3;
+		
+		switch (mode) {
+		case 1://button A does field oriented arcade
+			driveArcade();
+			break;
+		case 2://button B does just arcade
+			chassis.arcadeDrive(leftX, .75*xbox.getRawAxis(porting.rXAxis));
+			getHeading();
+			getDistance();
+			getAccel();
+			break;
+		case 3://button X does tank drive
+			driveTank();
+			break;
+		default: break;
+		}
+	}
+	
+	//drive with arcade drive with respect to the field. Left joystick only
+	public void driveArcade() {		
+		double radians = getHeading()*Math.PI/180;//convert angle to radian value
+		
+		getHeading();//update gyro
+		getDistance();//update sonar
+		getAccel();//update accelerometer
+		
+		//math to find how to turn robot to make field oriented work
+		double temp = Math.cos(radians)*leftX + Math.sin(radians)*leftY;
+		leftY = -leftX*Math.sin(radians) + leftY*Math.cos(radians);
+		leftX=temp;
+						
+		chassis.arcadeDrive(this.dampen*leftX, this.dampen*leftY);//drive
+		
+		if(Math.abs(xbox.getRawAxis(porting.rXAxis)) > .25) {
+			chassis.arcadeDrive(0, xbox.getRawAxis(porting.rXAxis));
+		}
+		
+	}
+	
+	//drive with tank drive. left and right joysticks
+	public void driveTank() {		
+		getHeading();//update gyr0
+		getDistance();//update sonar
+		getAccel();//update accelrometer
+		
+		//drive
+		chassis.tankDrive(xbox.getRawAxis(porting.rYAxis),xbox.getRawAxis(porting.lXAxis) );
+	}
+	
+	//updates the value of the sonar sensor
+	public void getDistance() {
+		this.currentDistance1 = m_ultrasonic1.getValue()*this.kValueToInches;
+		SmartDashboard.putNumber("distance right", currentDistance1);
+		
+		this.currentDistance2 = m_ultrasonic2.getValue()*this.kValueToInches;
+		SmartDashboard.putNumber("distance left", currentDistance2);
+	}
+	
+
+	//updates the value of the gyro
+	public double getHeading() {
+		SmartDashboard.putNumber("gyroAngle", gyro.getAngle());
+		return gyro.getAngle();
+	}
+	
+	//updates the value of the accelerometer Ignore for now
+	public double[] getAccel() {
+		accVal[0] = accel.getX()*9.81;
+		accVal[1] = accel.getY()*9.81;
+		accVal[2] = accel.getZ()*9.81;
+		SmartDashboard.putNumber("x accel", accVal[0]);
+		SmartDashboard.putNumber("y accel", accVal[1]);
+		SmartDashboard.putNumber("z accel", accVal[2]);
+		return accVal;
+	}
+}
---
+++ chassisTank/src/org/usfirst/frc/team5057/robot/Robot.java
@@ -0,0 +1,434 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package org.usfirst.frc.team5057.robot;
+
+import org.opencv.core.Rect;
+import org.opencv.imgproc.Imgproc;
+
+import edu.wpi.cscore.UsbCamera;
+import edu.wpi.first.wpilibj.ADXRS450_Gyro;
+import edu.wpi.first.wpilibj.AnalogAccelerometer;
+import edu.wpi.first.wpilibj.AnalogGyro;
+import edu.wpi.first.wpilibj.AnalogInput;
+import edu.wpi.first.wpilibj.AnalogPotentiometer;
+import edu.wpi.first.wpilibj.BuiltInAccelerometer;
+import edu.wpi.first.wpilibj.CameraServer;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.GenericHID.Hand;
+import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.Relay;
+import edu.wpi.first.wpilibj.Servo;
+import edu.wpi.first.wpilibj.Spark;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.Talon;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.interfaces.Accelerometer;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.vision.VisionThread;
+
+/**
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to each mode, as described in the IterativeRobot
+ * documentation. If you change the name of this class or the package after
+ * creating this project, you must also update the build.properties file in the
+ * project.
+ */
+public class Robot extends IterativeRobot {
+	private static final String kDefaultAuto = "Default";
+	private static final String kCustomAuto = "My Auto";
+	private String m_autoSelected;
+	private SendableChooser<String> m_chooser = new SendableChooser<>();
+	
+	//ports
+	final int leftDrivePort=0;
+	final int rightDrivePort=1;
+	final int liftPort=2;//change
+	
+	//driveTrain
+	DifferentialDrive chassis;
+	Spark leftMotor = new Spark(leftDrivePort);
+	Spark rightMotor = new Spark(rightDrivePort);
+	JoystickLocations porting = new JoystickLocations();
+	XboxController xbox = new XboxController(porting.joystickPort);
+	DriveTrain dtr;
+	
+	//lift
+	int directionLift = 0;
+	
+	/*
+	//lift
+	Spark lift = new Spark(liftPort);
+	int encoderPort = 10;//change
+	Encoder roller=new Encoder(encoderPort,encoderPort,false,Encoder.EncodingType.k4X);//check type
+	*/
+	
+	//vision init. This is code we will use for the vision programming. Ignore for now
+	VisionThread visionT;
+	UsbCamera cam;
+	double centerX=0.0;
+	double centerX1;
+	double centerX2;
+	final Object imgLock = new Object();
+	Relay LED;
+	int relayPort = 3;
+
+	//int uServo = 9;
+	//int lServo = 8;
+	
+
+	boolean upButton = false;
+
+	boolean downButton = false;
+
+	int positionCase = 0;
+
+	Spark lift = new Spark(liftPort);
+
+	int encoderPort1 = 0;
+
+	int encoderPort2 = 1;
+
+	Encoder enc=new Encoder(encoderPort1,encoderPort2,false,Encoder.EncodingType.k4X);
+
+	double distance;
+
+	double period;
+
+	double rate;
+
+	boolean direction;
+
+	boolean stopped;
+	
+	//things robot needs to do on startup
+	public void robotInit() {
+		//choose which autonomous to use
+		m_chooser.addDefault("Default Auto", kDefaultAuto);
+		m_chooser.addObject("My Auto", kCustomAuto);
+		SmartDashboard.putData("Auto choices", m_chooser);
+		
+		//setup drivetrain
+		chassis = new DifferentialDrive(leftMotor, rightMotor);
+		chassis.setExpiration(.1);
+		chassis.setSafetyEnabled(false);
+		
+		dtr = new DriveTrain(chassis, xbox, porting);
+		
+		//setup gyro
+		dtr.gyro.calibrate();
+		dtr.gyro.reset();
+
+		enc.setMaxPeriod(.1);
+
+		enc.setMinRate(10);
+
+		enc.setDistancePerPulse(0.71);
+
+		enc.setReverseDirection(true);
+
+		enc.setSamplesToAverage(7);
+
+		enc.reset();
+		
+		//vision code init
+		LED = new Relay(relayPort);
+		LED.set(Relay.Value.kForward);
+		cam= CameraServer.getInstance().startAutomaticCapture(0);//create new camera instance
+		cam.setResolution(320*2,240*2);//control pixel size
+		visionT=new VisionThread(cam, new GripPipeline(), pipeline -> {
+			if (!pipeline.filterContoursOutput().isEmpty()) {//check to see if image is there. If image:
+				Rect r1 = Imgproc.boundingRect(pipeline.filterContoursOutput().get(0));//left contour
+				Rect r2 = Imgproc.boundingRect(pipeline.filterContoursOutput().get(1));//right contour
+				synchronized(imgLock) {
+            		centerX1 = r1.x + (r1.width / 2);//find x of left
+            		centerX2 = r2.x + (r2.width/2);//find x or right
+            		centerX = (centerX1 + centerX2)/2;//average them together
+            	}SmartDashboard.putNumber("centerX", centerX);//publish to dashboard
+			}else {//if image doesnt exist
+				synchronized (imgLock) {
+	    		centerX = -1;//center isn't available
+	    		}
+	    		SmartDashboard.putNumber("centerX", centerX);//publish
+	    	}
+			
+			if(isAutonomous()){//if autonomous, allow camera to take lot of CPU
+	        	Timer.delay(.01);
+	        }
+	        else{//if not auto, don't let camera run often
+	            Timer.delay(0.1);
+	        }
+			
+		});
+		visionT.start();
+	}
+
+	/**
+	 * This autonomous. You can add additional auto modes by adding additional comparisons to
+	 * the switch structure below with additional strings. If using the
+	 * SendableChooser make sure to add them to the chooser code above as well.
+	 */
+	@Override
+	public void autonomousInit() {
+		m_autoSelected = m_chooser.getSelected();
+		System.out.println("Auto selected: " + m_autoSelected);
+		dtr.chassis.setSafetyEnabled(true);
+		//This is default code we use to choose an autonomous
+	}
+
+	/**This function is called periodically during autonomous.**/
+	@Override
+	public void autonomousPeriodic() { 
+		switch (m_autoSelected) {
+			case kCustomAuto://testing autonomous
+				autoSonar();
+				break;
+			case kDefaultAuto://nothing right now
+				SmartDashboard.putNumber("centerX",centerX);
+				LED.set(Relay.Value.kForward);
+				break;
+			default:
+				// Put default auto code here
+				break;
+			
+		}Timer.delay(.005);
+	}
+	
+	//temporary autonomous to drive with sonar
+	public void autoSonar() {
+		dtr.chassis.setSafetyEnabled(true);//allow auto to happen
+		dtr.getHeading();
+		dtr.getDistance();
+		dtr.getAccel();
+		dtr.updateAxes();
+			
+		/*if (Math.round(currentDistance1)!=Math.round(currentDistance2)) {
+		if (currentDistance1>currentDistance2) {
+			rightMotor.set(.2);
+		}else if (currentDistance1>currentDistance2) {
+			leftMotor.set(-.2);
+		}}*/
+		//else {
+		if(dtr.currentDistance1>18) {//if the robot is more than 18 inches from the wall
+			SmartDashboard.putNumber("AutoDriveOn",0);//say robot  moving
+			dtr.chassis.arcadeDrive(.5, 0);
+		}else {
+			SmartDashboard.putNumber("AutoDriveOn", 1);//say robot not moving
+			dtr.chassis.arcadeDrive(0, 0);
+		}
+		
+		SmartDashboard.putNumber("leftSpeed", leftMotor.get()*-1);
+		SmartDashboard.putNumber("rightSpeed", rightMotor.get());
+	}
+	
+	/**This function is called periodically during operator control.*/
+	@Override
+	public void teleopPeriodic() {
+		dtr.chassis.setSafetyEnabled(true);
+		dtr.changeDrive();
+		dtr.updateAxes();
+		LED.set(Relay.Value.kForward);
+		getEnc();
+		buttons();
+		//teleopLift();
+		
+	}
+	
+	/**This function is called periodically during test mode**/
+	@Override
+	public void testPeriodic() {}
+	
+	public void buttons() {
+		
+
+		if(downButton == false && xbox.getRawButton(porting.butLBumper) == true)
+
+		{
+
+			if(positionCase != 0)
+
+				positionCase--;
+
+			directionLift = 0;
+
+		}
+
+		else if(upButton == false && xbox.getRawButton(porting.butRBumper) == true)
+
+		{
+
+			if(positionCase != 6)
+
+				positionCase++;
+
+			directionLift = 1;
+
+		}
+
+		SmartDashboard.putNumber("positionCase", positionCase);
+
+		
+
+		if(directionLift == 0)
+
+		{
+
+			switch(positionCase) {
+
+			case 0: 
+
+				lift.set(0);
+
+				break;
+
+			case 1:
+
+				if(enc.getDistance() > 0)
+
+					lift.set(-1);
+
+				else
+
+					lift.set(0);
+
+				break;
+
+			case 2:
+
+				if(enc.getDistance() > 20*4)
+
+					lift.set(-1);
+
+				else
+
+					lift.set(0);
+
+				break;
+
+			case 3:
+
+				if(enc.getDistance() > 90*4)
+
+					lift.set(-1);
+
+				else
+
+					lift.set(0);
+
+				break;
+
+			case 4:
+
+				if(enc.getDistance() > 110*4)
+
+					lift.set(-1);
+
+				else
+
+					lift.set(0);
+
+				break;
+
+			
+
+			}
+
+		}
+
+		
+
+		if(directionLift == 1)
+
+		{
+
+			switch(positionCase) {
+
+			case 2:
+
+				if(enc.getDistance() < 20*4)
+
+					lift.set(1);
+
+				else
+
+					lift.set(0);
+
+				break;
+
+			case 3:
+
+				if(enc.getDistance() < 90*4)
+
+					lift.set(1);
+
+				else
+
+					lift.set(0);
+
+				break;
+
+			case 4:
+
+				if(enc.getDistance() < 110*4)
+
+					lift.set(1);
+
+				else
+
+					lift.set(0);
+
+				break;
+
+			case 5:
+
+				if(enc.getDistance() < 180*4)
+
+					lift.set(1);
+
+				else
+
+					lift.set(0);
+
+				break;
+
+			case 6:
+
+				lift.set(0);
+
+				break;
+
+			}
+		}
+
+	}
+	
+	public void getEnc() {
+
+		distance = enc.getDistance();
+
+		rate = enc.getRate();
+
+		direction = enc.getDirection();
+
+		stopped = enc.getStopped();
+
+		SmartDashboard.putNumber("distance", distance);
+
+		SmartDashboard.putBoolean("direction",direction);
+
+		SmartDashboard.putNumber("rate",rate);
+
+		SmartDashboard.putBoolean("stopped?",stopped);
+
+		
+
+	}
+}
